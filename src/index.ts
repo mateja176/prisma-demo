701b3ce25f69156695cb3106f85ed2a1c259d99b
Mateja Petrovic <ma.petrovic@levi9.com>
2019-09-28 21:00:38 +0200

    Query relational data

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,10 @@
-import { prisma } from './generated/prisma-client'
+import { prisma } from './generated/prisma-client';
 
 // A `main` function so that we can use async/await
 async function main() {
-  // Create a new user with a new post
-  const newUser = await prisma.createUser({
-    name: 'Bob',
-    email: 'bob@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Join us for GraphQL Conf in 2019',
-        },
-        {
-          title: 'Subscribe to GraphQL Weekly for GraphQL news',
-        },
-      ],
-    },
-  })
-  console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
-
-  // Read all users from the database and print them to the console
-  const allUsers = await prisma.users()
-  console.log(allUsers)
-
-  const allPosts = await prisma.posts()
-  console.log(allPosts)
+  // Read the previously created user from the database and print their posts to the console
+  const postsByUser = await prisma.user({ email: 'bob@prisma.io' }).posts();
+  console.log(`All posts by that user: ${JSON.stringify(postsByUser)}`);
 }
 
-main().catch(e => console.error(e))
+main().catch(e => console.error(e));
